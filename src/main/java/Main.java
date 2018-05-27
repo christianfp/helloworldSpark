bbfe9748c40dad7a6607fc75ab4459c79ed3217e
christianfp <christian_flores995@hotmail.com>
2018-05-27 14:13:14 -0400

    pom final2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,4 +4,11 @@ public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
     }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
